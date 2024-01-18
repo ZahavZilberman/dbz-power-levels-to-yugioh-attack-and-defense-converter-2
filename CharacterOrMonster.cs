74c674ed24c7e78c55423b45c7cd04362997abcc
Zahav Zilberman <zahavzilberman@gmail.com>
2024-01-18 15:21:15 +0200

    Add project files.

---
+++ CharacterOrMonster.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbz_power_levels_to_yugioh_attack_and_defense_converter_2
+{
+    public class CharacterOrMonster
+    {
+    public double ATKPoints { get; set; }
+    public double DEFPoints { get; set; }
+    public string Name { get; set; }
+    public double PowerLevel { get; set; }
+    public bool IsTheStrongestCharacterInHisDeck { get; set; }
+    public bool IsTheWeakestCharacterInHisDeck { get; set; }
+    public Deck TheDeckThisGuyBelongsTo { get; set; }
+
+        public CharacterOrMonster()
+        {
+            ATKPoints = 0.1;
+            DEFPoints = 0.1;
+            Name = "";
+            PowerLevel = 0;
+            IsTheStrongestCharacterInHisDeck = false;
+            IsTheWeakestCharacterInHisDeck = false;
+        }
+
+    }
+}
---
+++ Deck.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace dbz_power_levels_to_yugioh_attack_and_defense_converter_2
+{
+    public class Deck
+    {
+
+        #region Properties
+        public List<CharacterOrMonster> CharacterList { get; set; }
+        public int NumberOfMonstersStoredInIt { get; set; }
+        public CharacterOrMonster TheMonsterWithTheHighestPowerLevel { get; set; }
+        public CharacterOrMonster TheMonsterWithTheLowestPowerLevel { get; set; }
+        public double TheHighestAllowedATKPoints { get; set; }
+        public double TheLowestAllowedATKPoints { get; set; }
+        public double ThePowerLevelGapBetweenTheStrongestAndWeakest { get; set; }
+        public double ThePowerConvertorBetweenPowerLevelToATKPoints { get; set; }
+        public string DeckName { get; set; }
+        #endregion
+
+        #region Ctor
+        public Deck()
+        {
+            CharacterList = new List<CharacterOrMonster>();
+            TheMonsterWithTheHighestPowerLevel = new CharacterOrMonster();
+            TheMonsterWithTheLowestPowerLevel = new CharacterOrMonster();
+            NumberOfMonstersStoredInIt = 0;
+            TheHighestAllowedATKPoints = 0;
+            TheLowestAllowedATKPoints = 0;
+            
+            CharacterList.Add(TheMonsterWithTheLowestPowerLevel);
+            CharacterList.Add(TheMonsterWithTheHighestPowerLevel);
+            DeckName = "";
+        }
+        #endregion
+
+        #region Updating the properties from the deck's XML file infromation
+        public void ReadDeckXMLFile(string DeckDocPath)
+        {
+            if (DeckDocPath == null)
+            {
+                throw new Exception("WTF?");
+            }
+            XDocument DeckDoc = new XDocument(XDocument.Load(DeckDocPath));
+            XElement root = DeckDoc.Root;
+            XElement DeckNameElement = root.Element("DeckName");
+            XElement MonstersListElement = root.Element("CharacterList");
+            IEnumerable<XElement> MonstersList = MonstersListElement.Elements("Monster");
+            foreach(XElement monster in MonstersList)
+            {
+                if(monster.Element("TheMonsterWithTheLowestPowerLevel") != null)
+                {
+                    XElement MonsterElement = monster.Element("TheMonsterWithTheLowestPowerLevel");
+                    this.TheMonsterWithTheLowestPowerLevel.Name = MonsterElement.Element("Name").Value;
+                    this.TheMonsterWithTheLowestPowerLevel.PowerLevel = double.Parse(MonsterElement.Element("PowerLevel").Value);
+                    this.TheMonsterWithTheLowestPowerLevel.ATKPoints = double.Parse(MonsterElement.Element("ATKPoints").Value);
+                    this.TheMonsterWithTheLowestPowerLevel.DEFPoints = double.Parse(MonsterElement.Element("DEFPoints").Value);
+                    this.TheMonsterWithTheLowestPowerLevel.IsTheStrongestCharacterInHisDeck = false;
+                    this.TheMonsterWithTheLowestPowerLevel.IsTheWeakestCharacterInHisDeck = true;
+
+                    this.TheMonsterWithTheLowestPowerLevel.TheDeckThisGuyBelongsTo = new Deck();
+                    this.TheMonsterWithTheLowestPowerLevel.TheDeckThisGuyBelongsTo.DeckName = MonsterElement.Element("TheDeckThisGuyBelongsTo").Value;
+                }
+                else if(monster.Element("TheMonsterWithTheHighestPowerLevel") != null)
+                {
+                    XElement MonsterElement = monster.Element("TheMonsterWithTheHighestPowerLevel");
+                    this.TheMonsterWithTheHighestPowerLevel.Name = MonsterElement.Element("Name").Value;
+                    this.TheMonsterWithTheHighestPowerLevel.PowerLevel = double.Parse(MonsterElement.Element("PowerLevel").Value);
+                    this.TheMonsterWithTheHighestPowerLevel.ATKPoints = double.Parse(MonsterElement.Element("ATKPoints").Value);
+                    this.TheMonsterWithTheHighestPowerLevel.DEFPoints = double.Parse(MonsterElement.Element("DEFPoints").Value);
+                    this.TheMonsterWithTheHighestPowerLevel.IsTheStrongestCharacterInHisDeck = true;
+                    this.TheMonsterWithTheHighestPowerLevel.IsTheWeakestCharacterInHisDeck = false;
+
+                    this.TheMonsterWithTheHighestPowerLevel.TheDeckThisGuyBelongsTo = new Deck();
+                    this.TheMonsterWithTheHighestPowerLevel.TheDeckThisGuyBelongsTo.DeckName = MonsterElement.Element("TheDeckThisGuyBelongsTo").Value;
+                }
+                else
+                {
+                    CharacterOrMonster AnotherMonster = new CharacterOrMonster();
+                    AnotherMonster.Name = monster.Element("Name").Value;
+                    AnotherMonster.PowerLevel = double.Parse(monster.Element("PowerLevel").Value);
+                    AnotherMonster.ATKPoints = double.Parse(monster.Element("ATKPoints").Value);
+                    AnotherMonster.DEFPoints = double.Parse(monster.Element("DEFPoints").Value);
+                    AnotherMonster.IsTheStrongestCharacterInHisDeck = false;
+                    AnotherMonster.IsTheWeakestCharacterInHisDeck = false;
+                    AnotherMonster.TheDeckThisGuyBelongsTo = new Deck();
+                    AnotherMonster.TheDeckThisGuyBelongsTo.DeckName = monster.Element("TheDeckThisGuyBelongsTo").Value;
+                    CharacterList.Add(AnotherMonster);
+                }
+            }
+
+            this.TheHighestAllowedATKPoints = double.Parse(root.Element("TheHighestAllowedATKPoints").Value);
+            this.TheLowestAllowedATKPoints = double.Parse(root.Element("TheLowestAllowedATKPoints").Value);
+            this.ThePowerLevelGapBetweenTheStrongestAndWeakest = double.Parse(root.Element("ThePowerLevelGapBetweenTheStrongestAndWeakest").Value);
+            this.ThePowerConvertorBetweenPowerLevelToATKPoints = double.Parse(root.Element("ThePowerConvertorBetweenPowerLevelToATKPoints").Value);
+            this.DeckName = DeckNameElement.Value;
+        }
+
+        #endregion
+    }
+}
